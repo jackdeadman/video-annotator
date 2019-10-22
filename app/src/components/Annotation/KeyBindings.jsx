45299b1b335b268dcd94bb74223eaaebc82d9c8e
Jack Deadman <jdeadman1@sheffield.ac.uk>
2019-10-22 16:51:55 +0100

    Select speaker with number keys

--- app/src/components/Annotation/KeyBindings.jsx
+++ app/src/components/Annotation/KeyBindings.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react';
 
 import { REMOVE_ANNOTATION, SET_SELECTED_SPEAKER } from '../../constants/actionTypes';
-import { DELETE, ESCAPE, E_KEY } from '../../constants/keycodes';
+import {
+    DELETE, ESCAPE, E_KEY,
+    ONE_KEY, TWO_KEY, THREE_KEY, FOUR_KEY
+} from '../../constants/keycodes';
 import { useKeyboard } from '../../hooks/keyboard';
 
 const KeyBindings = ({ children, state, dispatch, callbacks }) => {
     const keyPressed = useKeyboard();
-    const { annotations, selectedSpeaker } = state;
+    const { annotations, selectedSpeaker, speakers } = state;
     const selectedAnnotations = annotations[state.selectedFrame];
     const { onEdit } = callbacks;
+
+    // TODO: Add keybindings to hide all but selected
 
     useEffect(function() {
         switch (keyPressed) {
@@ -29,6 +34,16 @@ const KeyBindings = ({ children, state, dispatch, callbacks }) => {
                 break
             case E_KEY:
                 onEdit(state.selectedSpeaker);
+                break;
+            case ONE_KEY:
+            case TWO_KEY:
+            case THREE_KEY:
+            case FOUR_KEY:
+                dispatch({
+                    type: SET_SELECTED_SPEAKER,
+                    value: speakers[keyPressed-ONE_KEY]
+                })
+                break;
         }
     }, [keyPressed]);
 
--- app/src/constants/keycodes.js
+++ app/src/constants/keycodes.js
@@ -4,3 +4,9 @@ export const ESCAPE = 27;
 
 // Letters
 export const E_KEY = 69;
+
+// Numbers
+export const ONE_KEY = 49;
+export const TWO_KEY = 50;
+export const THREE_KEY = 51;
+export const FOUR_KEY = 52;
